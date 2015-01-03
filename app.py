86bb27474ab089d4f7c828020ba941af4dba1825
Deepak Mohandas <deepak@plivo.com>
2015-01-03 02:29:59 +0000

    better responses

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, url_for, make_response
+from flask import Flask, request, redirect, url_for, make_response, jsonify
 from werkzeug import secure_filename
 from pymongo import Connection
 from bson.objectid import ObjectId
@@ -26,9 +26,13 @@ def upload_file():
 	print grid
 	oid = grid.put(file,content_type=file.content_type,filename="%s" %filename)
 	if oid:
-	  return "File succesfuly uploaded, refid: %s" %oid
+ 	  response = jsonify(result={"status": 201, "description": "File succesfuly uploaded, RefID: %s" %oid})
+	  response.status_code = 201
+	  return response
 	else:
-	  return HTTPError(500, "Unable to upload the file")
+	  response = jsonify(result={"status": 500, "description": "UH OH Something Went Wrong :("})
+	  response.status_code = 500
+	  return response
 
 @app.route('/search/oid/<oid>', methods=['POST','GET'])
 def get_files_by_oid(oid):
@@ -37,23 +41,23 @@ def get_files_by_oid(oid):
 	grid = mongo_conn()
 	try:
 	  file = grid.get(ObjectId(mongo_oid))
-	  print file
           return Response(file, mimetype=file.content_type, direct_passthrough=True)
-	except Exception:
-#        except NoFile:
-          return HTTPError(404, "Not found:")
-
+	except Exception, e:
+          response = jsonify(result={"status": 404, "description": "UH OH File not Found :("})
+          response.status_code = 404
+          return response
 @app.route('/search/file/<filename>', methods=['POST','GET'])
 def get_files_by_filename(filename):
     if request.method == 'GET':
         mongo_filename = filename
         grid = mongo_conn()
         try:
-	  print "file -> %s" %mongo_filename
           file = grid.find({"filename": "%s" %mongo_filename}).sort('uploadDate', -1).limit(1)[0]
           return Response(file, mimetype=file.content_type, direct_passthrough=True)
-        except NoFile:
-          return HTTPError(404, "Not found:" + mongo_filename)
+	except Exception, e:
+	  response = jsonify(result={"status": 404, "description": "UH OH File not Found :("})
+	  response.status_code = 404
+	  return response
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
